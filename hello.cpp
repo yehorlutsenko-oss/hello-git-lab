b919f9e35461ca21420db602d8e5f407f506fee6
Yehor Lutsenko <yehor.lutsenko@student.karazin.ua>
2026-02-01 17:06:07 +0200

    Master changes first line

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main() {
     cout << "This is my first Git project!" << endl;
     cout << "Git is awesome!" << endl;	
     cout << "This is feature branch!" << endl;
+    cout << "Hello from MASTER!" << endl;
     return 0;
 }
